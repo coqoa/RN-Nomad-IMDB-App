096e3d1474a322072bd702c921408a18c6d1cfcd
coqoa <choibm0208@gmail.com>
2022-02-15 23:54:13 +0900

    220215 RN 기초작업

--- App.js
+++ App.js
@@ -4,6 +4,13 @@ import * as Font from "expo-font";
 import { Text } from 'react-native';
 import {Ionicons} from "@expo/vector-icons"
 import {Asset} from 'expo-asset';
+import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
+import Stack from './navigation/Stack';
+import Root from './navigation/Root';
+import { useColorScheme } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './navigation/styled';
 
 const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font))
 const loadImages = (images) => images.map(image => {
@@ -22,6 +29,7 @@ export default function App() {
     // const images = loadImages([require('./Local'),'url'])
     await Promise.all(...fonts, ...images)
   }; 
+  const isDark = useColorScheme() === "dark" ;
   if(!ready){
     return (
     <AppLoading
@@ -31,6 +39,12 @@ export default function App() {
     );
   }
   return (
-    <Text>We are done!!!!</Text>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+        {/* <Tabs /> */}
+        {/* <Stack /> */}
+      </NavigationContainer>
+    </ThemeProvider>
   )
 }
